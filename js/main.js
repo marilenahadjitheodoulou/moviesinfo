617b0cc8bf65db9e4e6d35e417c4bf4ac15b0035
marilenahadjitheodoulou <it216102@hua.gr>
2021-01-04 23:25:56 +0200

    second commit

--- js/main.js
+++ js/main.js
@@ -5,25 +5,33 @@ $(document).ready(() => {
     e.preventDefault();
   });
 });
+
+$(document).ready(() => {
+  $('#searchOrm').on('submit', (e) => {
+    let ratingText = $('#ratingText').val();
+    getRatings(ratingText);
+    e.preventDefault();
+  });
+});
 // Add a 401 response interceptor
 window.axios.interceptors.response.use(function (response) {
   return response;
 }, function (error) {
   if (401 === error.response.status) {
-      // handle error: inform user, go to login, etc
+    // handle error: inform user, go to login, etc
   } else {
-      return Promise.reject(error);
+    return Promise.reject(error);
   }
 });
 
-function getMovies(searchText){
-  axios.get('http://62.217.127.19:8010/movie/'+searchText)
+function getMovies(searchText) {
+  axios.get('http://62.217.127.19:8010/movie/' + searchText)
     .then((response) => {
       console.log(response);
       let movies = response.data;
       let output = '';
       $.each(movies, (index, movie) => {
-        output +=  `
+        output += `
         <div class="col-md-5">
         <div class="well text-center">
         <h5>Movies Details</h5>
@@ -42,5 +50,37 @@ function getMovies(searchText){
     });
 }
 
-
-
+function getRatings(ratingText) {
+  axios.get('http://62.217.127.19:8010/ratings/' + ratingText)
+    .then((response) => {
+      console.log(response);
+      let ratings = response.data;
+      let output = '';
+      $.each(ratings, (index, rating) => {
+        output += `
+        <table class="table table-bordered" style="width:100%">
+          <thead class="thead-light">
+            <tr>
+              <th>UserId</th>
+              <th>MovieId</th>
+              <th>Rating</th>
+              <th>Timestamp</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>${rating.userId}</td>
+              <td>${rating.movieId}</td>
+              <td>${rating.rating}</td>
+              <td>${rating.timestamp}</td>
+            </tr>
+          </tbody>
+        </table>
+        `;
+      })
+      $('#ratings').html(output);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
