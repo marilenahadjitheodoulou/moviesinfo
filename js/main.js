cb52d0edd10c59237d6b58280a7a2785c5c271fa
marilenahadjitheodoulou <it216102@hua.gr>
2021-01-07 20:51:43 +0200

    r commit

--- js/main.js
+++ js/main.js
@@ -5,14 +5,6 @@ $(document).ready(() => {
     e.preventDefault();
   });
 });
-/**
-Use event delegation to link to drop down to the rate function
-*/
-$('.container').on('change', '.movie-rating', function () {
-  var rating = $(this).val();
-  var movieId = $(this).attr('data-id');
-  rateMovie(movieId, rating);
-});
 
 // Add a 401 response interceptor
 window.axios.interceptors.response.use(function (response) {
@@ -35,53 +27,18 @@ function getMovies(searchText) {
       let output = '';
       $.each(movies, (index, movie) => {
         output += `
-    <div class="col-md-5">
-    <div class="well text-center">
-    <li class="list-group-item"><strong>MovieId:</strong> ${movie.movieId}</li>
-    <li class="list-group-item"><strong>Title:</strong> ${movie.title}</li>
-    <li class="list-group-item"><strong>Genres:</strong> ${movie.genres}</li>
-    </div>
-    <select class="movie-rating" data-id="{{movieId}}">
-      <option>Rate</option>
-      <option value="1">1</option>
-      <option value="2">2</option>
-      <option value="3">3</option>
-      <option value="4">4</option>
-      <option value="5">5</option>
-      <option value="delete">Delete</option>
-  </select>
-    </div>
-    `;
+      <div class="col-md-5">
+        <div class="well text-center">
+          <li class="list-group-item"><strong>MovieId:</strong> ${movie.movieId}</li>
+          <li class="list-group-item"><strong>Title:</strong> ${movie.title}</li>
+          <li class="list-group-item"><strong>Genres:</strong> ${movie.genres}</li>
+        </div>
+      </div>
+      `;
       })
       $('#movies').html(output);
     })
     .catch((err) => {
       console.log(err);
     });
-}
-
-function rateMovie(movieId, movieRating) {
-  if (movieRating === 'delete') {
-    deleteRating(movieId);
-  } else {
-    axios.post('http://62.217.127.19:8010/ratings/', {
-      keyword: movieId
-    })
-    .then((response) => {
-      console.log(response);
-    }, (error) => {
-        console.log(error);
-      });
-  }
-}
-
-function deleteRating(movieId){
-  axios.post('http://62.217.127.19:8010/ratings/', {
-      keyword: movieId
-    })
-  .then((response) => {
-    console.log(response);
-  }, (error) => {
-    console.log(error);
-  });
 }
